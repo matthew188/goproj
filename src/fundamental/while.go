4a3105af193a8a901d20748916d42a8e4d1b0cd9
matt <matt18@foxmial.com>
2018-04-01 12:11:21 +0800

    fundamental

--- src/fundamental/while.go
+++ src/fundamental/while.go
@@ -5,8 +5,8 @@ import "fmt"
 func UseWhileFindPrim() {
 	var C, c int //声明变量
 	C = 1        /*初会将C的值变为1，当我们goto A时for语句会重新执行（不是重新一轮循环）*/
-A:
-	for C < 100 {
+  A:
+	for C < 50 {
 		C++ //C=1不能写入for这里就不能写入
 		for c = 2; c < C; c++ {
 			if C%c == 0 {
