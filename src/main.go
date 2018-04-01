b83c3a3e61aa645eacb84c908f61177eb12424d1
matt <matt18@foxmial.com>
2018-04-01 11:40:17 +0800

    init

---
+++ src/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"github.com/matthew188/LearnTest/golang/code/fundamental"
+)
+
+func main() {
+	fmt.Println("hello")
+
+	//常量测试
+	fundamental.Test_const();
+
+	//for质数测试
+	fundamental.UseWhileFindPrim();
+
+	println(1 <<10)
+
+
+	//i:=1
+	//for true  {
+	//	i++
+	//	fmt.Printf("这是无限循环。%d\n",i);
+	//}
+
+}
